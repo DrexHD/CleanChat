a516aff8c9ef3f16034f937a9e8c360dc01e6a78
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2023-11-19 10:17:12 +0100

    use MixinExtras for better mod compatibility

--- src/main/java/me/drex/cleanchat/mixin/ChatComponentMixin.java
+++ src/main/java/me/drex/cleanchat/mixin/ChatComponentMixin.java
@@ -1,5 +1,8 @@
 package me.drex.cleanchat.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import me.drex.cleanchat.CleanChatMod;
 import net.minecraft.client.GuiMessageTag;
 import net.minecraft.client.gui.GuiGraphics;
@@ -10,14 +13,14 @@ import org.spongepowered.asm.mixin.injection.*;
 @Mixin(ChatComponent.class)
 public abstract class ChatComponentMixin {
 
-    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/GuiMessageTag;icon()Lnet/minecraft/client/GuiMessageTag$Icon;"))
-    public GuiMessageTag.Icon removeIcon(GuiMessageTag original) {
-        return CleanChatMod.CHAT_CONFIG.disableIcons ? null : original.icon();
+    @ModifyExpressionValue(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/GuiMessageTag;icon()Lnet/minecraft/client/GuiMessageTag$Icon;"))
+    public GuiMessageTag.Icon removeIcon(GuiMessageTag.Icon original) {
+        return CleanChatMod.CHAT_CONFIG.disableIcons ? null : original;
     }
 
-    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;fill(IIIII)V", ordinal = 1))
-    public void removeBar(GuiGraphics guiGraphics, int i, int j, int k, int l, int m) {
-        if (!CleanChatMod.CHAT_CONFIG.disableBar) guiGraphics.fill(i, j, k, l, m);
+    @WrapOperation(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;fill(IIIII)V", ordinal = 1))
+    public void removeBar(GuiGraphics instance, int x1, int y1, int x2, int y2, int color, Operation<Void> original) {
+        if (!CleanChatMod.CHAT_CONFIG.disableBar) original.call(instance, x1, y1, x2, y2, color);
     }
 
     @ModifyArg(method = "render", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;translate(FFF)V", ordinal = 0), index = 0)
@@ -25,12 +28,12 @@ public abstract class ChatComponentMixin {
         return CleanChatMod.CHAT_CONFIG.disableBar ? 2 : original;
     }
 
-    @ModifyConstant(method = "screenToChatX", constant = @Constant(doubleValue = 4.0))
+    @ModifyExpressionValue(method = "screenToChatX", at = @At(value = "CONSTANT", args = "doubleValue=4.0"))
     public double adjustChatCoordinates(double original) {
         return CleanChatMod.CHAT_CONFIG.disableBar ? 2 : original;
     }
 
-    @ModifyConstant(method = "getTagIconLeft", constant = @Constant(intValue = 4))
+    @ModifyExpressionValue(method = "getTagIconLeft", at = @At(value = "CONSTANT", args = "intValue=4"))
     public int moveIcon(int original) {
         return CleanChatMod.CHAT_CONFIG.disableBar ? 2 : original;
     }
